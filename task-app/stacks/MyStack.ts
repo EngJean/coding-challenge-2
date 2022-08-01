b711a87171bed4467fac5237dcf56325e5b515a4
Jean Rodríguez <eng.jeanrodriguez@gmail.com>
2022-08-01 17:03:50 -0400

    Building front end

--- task-app/stacks/MyStack.ts
+++ task-app/stacks/MyStack.ts
@@ -7,10 +7,45 @@ import {
 
 export function MyStack({ stack }: StackContext) {
   // Create the table
-  const table = new Table(stack, "Counter", {
+  const table = new Table(stack, "Users", {
     fields: {
-      counter: "string",
+      userId: "string",
     },
-    primaryIndex: { partitionKey: "counter" },
+    primaryIndex: { partitionKey: "userId" },
+    stream: true,
+  });
+  table.attachPermissions(["s3"]);
+
+  // Create the HTTP API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        // Allow the API to access the table
+        permissions: [table],
+        // Pass in the table name to our API
+        environment: {
+          tableName: table.tableName,
+        },
+      },
+    },
+    routes: {
+      "POST /": "functions/lambda.handler",
+    },
+  });
+
+  // Deploy our React app
+  const site = new ReactStaticSite(stack, "ReactSite", {
+    path: "frontend",
+    environment: {
+      REACT_APP_API_URL: api.url,
+    },
+    // No custom domain just yet.
+    // customDomain: "www.tasks.com",
+  });
+
+  // Show the URLs in the output
+  stack.addOutputs({
+    SiteUrl: site.url,
+    ApiEndpoint: api.url,
   });
 }
